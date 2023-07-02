ae96182423b7a0acaa73ee0900d297e8093d1c3a
Abhi14499 <abhimishra14499@gmail.com>
2023-07-02 11:49:49 +0530

    TASK 3

--- dom2.js
+++ dom2.js
@@ -1,8 +1,3 @@
-const headerTitle = document.getElementById('header-title');
-
-
-headerTitle.style.border = '2px solid black';
-headerTitle.style.padding = '5px';
 const addItemsHeading = document.getElementById('add-items-heading');
 
 
